29168a2f73b8e730822d521f11b0002da646a233
charlotteisabella772 <charlotteisabella772@gmail.com>
2026-08-20 14:29:50 +0800

    Update RENEW_URLS with a new URL

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 
 # 配置区域
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=e34fff3a-fbc0-48f4-9747-c7a460d8cf45",
     # 添加更多链接
 ]
 
